f7f7956abb224be2523e388dd06911eca2102844
vincenzo <vincenzobianco1993@gmail.com>
2016-09-23 15:45:09 -0400

    update the add to cart controller/ push products

--- public/js/controllers/addToCartCtrl.js
+++ public/js/controllers/addToCartCtrl.js
@@ -1,7 +1,17 @@
 app.controller('addToCartCtrl',function ($scope, User) {
-
+  var me = $scope.user;
   $scope.addToCart = function (product) {
-    console.log("clicked",product);
-  }
+    var obj = { product: product._id, quantity: 1};
+    me.data.cart.push(obj);
+    User.cart(me.data.cart)
+      .then(function (user) {
+        $scope.success = true;
+        console.log(user.data);
+      })
+      .catch(function (err) {
+        $scope.success = false;
+        console.log(err);
+      })
 
-})
+  }
+});

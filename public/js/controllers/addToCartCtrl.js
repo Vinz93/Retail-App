f85a00b730910e370eba0dc3b8a1b1c0e6921959
Vincenzo Bianco <vincenzo_bianco_93@hotmail.com>
2016-09-25 11:35:38 -0400

    update issue quantity of a product

--- public/js/controllers/addToCartCtrl.js
+++ public/js/controllers/addToCartCtrl.js
@@ -1,20 +1,37 @@
 app.controller('addToCartCtrl',function ($scope, User) {
-  var me = $scope.user;
+  var cart = undefined;
+  var newItem = undefined;
+  $scope.success = false;
+  var me = $scope.user
 
-  $scope.addToCart = function (product) {
+  $scope.addToCart = function (product){
+    var newItem = true;
     var obj = { product: product._id, quantity: 1};
 
     if(!me){
       alert("you must be logged");
       return false;
     }
+    cart = me.data.cart.map(function (i) {
+      console.log(i.product._id,' ',obj.product);
+      console.log(i.product._id === obj.product);
+      if(i.product._id === obj.product){
+        i.quantity++;
+        newItem = false;
+        console.log('is new ? ', newItem)
+      }
+      return  i;
+    });
+    console.log('is new ? ', newItem);
+    if(newItem)
+      cart.push(obj);
 
-    me.data.cart.push(obj);
-
-    User.cart(me.data.cart)
+    User.cart(cart)
       .then(function (user) {
         $scope.success = true;
-        console.log(user);
+        $scope.user = user.data;
+        me = user.data;
+        console.log(user.data.data)
       })
       .catch(function (err) {
         $scope.success = false;

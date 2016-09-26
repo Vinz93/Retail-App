a45de6d22dcf45e1358fe296beb8ddb7bd248f8b
vincenzo <vincenzobianco1993@gmail.com>
2016-09-26 17:42:11 -0400

    checkout view and update quantity

--- public/js/controllers/checkoutCtrl.js
+++ public/js/controllers/checkoutCtrl.js
@@ -1,3 +1,22 @@
 app.controller('checkoutCtrl',function ($scope, User) {
+  User.getInfo()
+      .then(function(res) {
+          $scope.user = res.data;
+          $scope.subTotal = calculate(res.data.data.cart);
+      });
+
+      $scope.update = function () {
+          $scope.subTotal = calculate($scope.user.data.cart);
+          //call api with put;
+      };
+
+      function calculate(cart) {
+        var acum = 0;
+        cart.forEach(function(item) {
+            acum += item.product.internal.aproximatePriceUSD * item.quantity;
+        });
+        return Math.floor(acum * 100) / 100;
+      };
+
 
 });
